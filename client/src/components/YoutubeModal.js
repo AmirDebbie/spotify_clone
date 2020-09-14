dbf0f0fcec78886e160dcd194504acb0f98be1c6
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-09-14 16:49:10 +0300

    added version material ui

--- client/src/components/YoutubeModal.js
+++ client/src/components/YoutubeModal.js
@@ -50,9 +50,9 @@ export default function YoutubeModal({ youtube_link, title }) {
         width="560"
         height="315"
         src={`https://www.youtube.com/embed/${video_id}`}
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
