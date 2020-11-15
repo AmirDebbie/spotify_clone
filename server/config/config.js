4b05df702df13f6c84f4ad2acc63e953c6e14440
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-11-15 11:30:26 +0200

    changed config to js from json

---
+++ server/config/config.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: process.env.DB_PASS,
+    database: "spotify_orm",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    define: {
+      underscored: true,
+    },
+  },
+  test: {
+    username: "root",
+    password: process.env.DB_PASS,
+    database: "spotify_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    define: {
+      underscored: true,
+    },
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
