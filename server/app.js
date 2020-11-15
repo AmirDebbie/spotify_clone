c4f534c6a8e973053833c8b2f6156e54dbf0e6a8
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-11-15 23:32:42 +0200

    added cookie parser

--- server/app.js
+++ server/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use(
   morgan(function (tokens, req, res) {
